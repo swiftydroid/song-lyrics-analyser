9629814df7a30825c9e334be36c0a95b99e2da43
swiftydroid <andrewkhong84@gmail.com>
2021-01-31 13:53:05 +0800

    Exclude songs tagged with Version, Live and Remix

--- get_lyrics.py
+++ get_lyrics.py
@@ -3,7 +3,8 @@ from properties import GENIUS_API_TOKEN, ARTIST_NAME, SEARCH_MAX_SONGS, SONGS_FI
 
 
 def get_lyrics():
-    genius = lyricsgenius.Genius(GENIUS_API_TOKEN, remove_section_headers=True)
+    genius = lyricsgenius.Genius(GENIUS_API_TOKEN, remove_section_headers=True,
+                                 excluded_terms=['Ver', '(Live)', '(Remix)'])
 
     artist = genius.search_artist(ARTIST_NAME, max_songs=SEARCH_MAX_SONGS)
     artist.save_lyrics(filename=SONGS_FILENAME, overwrite=True)
