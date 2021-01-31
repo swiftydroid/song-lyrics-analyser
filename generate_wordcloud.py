c0331858fe43b8a51beb2a177e4779632481a3f3
swiftydroid <andrewkhong84@gmail.com>
2021-01-31 21:02:08 +0800

    Move the cleaning text codes to a new function

--- generate_wordcloud.py
+++ generate_wordcloud.py
@@ -9,12 +9,31 @@ from additional_stopwords import additional_stopwords
 from wordcloud import WordCloud
 
 
+def clean_text(text):
+    all_stopwords = stopwords.words('english')
+    all_stopwords.extend(additional_stopwords)
+
+    text = ' '.join(text).lower().replace('—', ' ').replace('-', ' ')
+
+    # remove all punctuation except for apostrophe since we can have words like 'cause, fallin' etc
+    text = re.sub(r"[^\w\d'\s]+", '', text)
+
+    # remove all numbers
+    text = re.sub(r'[0-9]', '', text)
+
+    # remove all stop words
+    text = word_tokenize(text)
+    text_tokens_without_sw = [word for word in text if word not in all_stopwords]
+
+    # join the tokens into a long string before passing to WordCloud
+    text = ' '.join(text_tokens_without_sw)
+
+    return text
+
+
 def generate_wordcloud():
     lyrics_org = []
     lyrics_translated = []
-
-    all_stopwords = stopwords.words('english')
-    all_stopwords.extend(additional_stopwords)
 
     with open(SONGS_FILENAME) as fp:
         data = json.load(fp)
@@ -27,17 +46,7 @@ def generate_wordcloud():
         translated_lyrics = translator.translate(lyrics, lang_tgt='en')
         lyrics_translated.append(translated_lyrics)
 
-    lyrics_translated = ' '.join(lyrics_translated).lower().replace('—', ' ').replace('-', ' ')
-
-    # remove all punctuation except for apostrophe since we can have words like 'cause, fallin' etc
-    lyrics_translated = re.sub(r"[^\w\d'\s]+", '', lyrics_translated)
-
-    # remove all stop words
-    lyrics_translated = word_tokenize(lyrics_translated)
-    lyrics_tokens_without_sw = [word for word in lyrics_translated if word not in all_stopwords]
-
-    # join the tokens into a long string before passing to WordCloud
-    lyrics_translated = ' '.join(lyrics_tokens_without_sw)
+    lyrics_translated = clean_text(lyrics_translated)
 
     wc = WordCloud(max_words=1000000, max_font_size=50, collocations=False).generate(lyrics_translated)
     wc_plt.figure()
